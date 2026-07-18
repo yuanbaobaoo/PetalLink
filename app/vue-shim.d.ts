ca52ec870a0125503c8e7b4960e065b9678dd70c
yuanbaobao <1021443805@qq.com>
2026-07-18 17:36:23 +0800

    # update

--- app/vue-shim.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Vue 单文件组件类型声明（TS 识别 .vue 文件）
- */
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
